e5cfe59b5817ba84e9d1ce2d724a9a74b72b442c
Phat <tvp14042005@gmail.com>
2025-12-25 04:14:26 +0700

    Revert "fix mot ti o hoa don"
    This reverts commit 15943d85075f03b53298158942639438de958ea7.

--- karaoke-nnice-api/src/main/java/com/nnice/karaoke/repository/HoaDonRepository.java
+++ karaoke-nnice-api/src/main/java/com/nnice/karaoke/repository/HoaDonRepository.java
@@ -3,12 +3,7 @@ package com.nnice.karaoke.repository;
 import com.nnice.karaoke.entity.HoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface HoaDonRepository extends JpaRepository<HoaDon, Integer> {
-    /**
-     * Tìm tất cả hóa đơn theo trạng thái
-     */
-    List<HoaDon> findByTrangThai(String trangThai);
 }
